a3192a9e14aaaa816c2ae9481d69cfcf3bdc0407
Augusto Rodrigues De Santana Neto <51301642+augustorsn@users.noreply.github.com>
2024-07-30 13:55:14 -0300

    first commit

---
+++ src/main/java/com/augustorsn/spring/auth/playground/playground/auth/PlaygroundAuthApplication.java
@@ -0,0 +1,13 @@
+package com.augustorsn.spring.auth.playground.playground.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/augustorsn/spring/auth/playground/playground/auth/PlaygroundAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustorsn.spring.auth.playground.playground.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
